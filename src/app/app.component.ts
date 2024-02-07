819d0ae431e0372f0d1e30f7247ef9e79eaef093
naushadwebdesigner <naushad.webdesigner@gmail.com>
2024-02-07 15:02:07 +0530

    Component setup

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,21 +1,12 @@
 import { Component } from '@angular/core';
-
-
-
+import { HeaderComponent } from './components/header/header.component';
+import { RouterLink, RouterOutlet } from '@angular/router';
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [],
+  imports: [ HeaderComponent, RouterLink, RouterOutlet ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-
-
-
- 
-
-
-
-
 }
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,2 +1,10 @@
 import { Routes } from '@angular/router';
-export const routes: Routes = [];
+import { HomeComponent } from './components/home/home.component';
+import { CartComponent } from './components/cart/cart.component';
+export const routes: Routes = [
+
+    {path:"", component:HomeComponent},
+    {path:"home", component:HomeComponent},
+    {path:"cart", component:CartComponent},
+
+];
