20d71dc146eef3a8d8e57e9cd7026aca9d756919
naushadwebdesigner <naushad.webdesigner@gmail.com>
2024-02-07 14:21:02 +0530

    app clean

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,11 @@
 import { Component } from '@angular/core';
-import { RouterLink, RouterOutlet } from '@angular/router';
-import { ProductsComponent } from './comp/products/products.component';
-import { NavbarComponent } from './comp/navbar/navbar.component';
 
 
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, RouterLink, ProductsComponent, NavbarComponent],
+  imports: [],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,12 +1,2 @@
 import { Routes } from '@angular/router';
-import { ProductsComponent } from './comp/products/products.component';
-import { CartComponent } from './comp/cart/cart.component';
-
-
-export const routes: Routes = [
-
-    { path: "", component:ProductsComponent },
-    { path: "cart", component:CartComponent },
-    
-    
-];
+export const routes: Routes = [];
